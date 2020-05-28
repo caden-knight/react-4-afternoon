b7a9c91286871d169ee22fdcdf89be9832a531d7
caden-knight <cadenknight@gmail.com>
2020-05-28 15:59:34 -0600

    the deed is done

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import Student from './components/Student/Student';
 export default (
 	<Switch>
 		<Route exact path="/" component={Home} />
-		<Route path="about" component={About} />
+		<Route path="/about" component={About} />
 		<Route path="/classlist/:class" component={ClassList} />
 		<Route path="/student/:id" component={Student} />
 	</Switch>
